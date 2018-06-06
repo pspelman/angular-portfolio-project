1ca6dd8bcdc7e42104ee5e4f59e82bef8826fb1b
pspelman <phil_spelman@hotmail.com>
2018-06-06 09:46:11 -0700

    Added foreground container for readability. Began project card styling.

--- ang-portfolio/src/app/app.component.ts
+++ ang-portfolio/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   main_page_title = 'My PortFoLiO';
+
+
+  public ngOnInit() {
+    $(function () {
+      // alert('ready');
+    });
+
+  }
 }
--- ang-portfolio/src/app/navbar/navbar.component.ts
+++ ang-portfolio/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,30 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(function () {
+      // When the user scrolls the page, execute myFunction
+      window.onscroll = function() {myFunction()};
+
+      // Get the nav__topnav
+      var nav__topnav = document.getElementById("nav__topnav");
+
+      // Get the offset position of the nav__topnav
+      var sticky = nav__topnav.offsetTop;
+      console.log(`sticky: `,sticky);
+      // Add the sticky class to the nav__topnav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+      function myFunction() {
+        if (window.pageYOffset >= sticky) {
+          nav__topnav.classList.add("sticky");
+          console.log(`making sticky`,);
+        } else {
+          nav__topnav.classList.remove("sticky");
+          console.log(`making un-sticky`,);
+        }
+      }
+    });
+
+
   }
 
 }
